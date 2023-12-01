4e50a02fdb9b030d976aa32f29b573f59cacccda
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-01 12:58:15 +0100

    Day One

---
+++ AdventOfCode/Day 1/Program.cs
@@ -0,0 +1,48 @@
+﻿using Utils;
+
+// Load input
+
+string input = InputLoader.LoadInput();
+
+// Part One
+
+int Func(string s)
+{
+    int result = 0;
+    foreach (var line in s.Split('\n').SkipLast(1))
+    {
+        char[] numbers = line.ToArray().Where(x => int.TryParse(x.ToString(), out _)).ToArray();
+        result += int.Parse(numbers[0].ToString() + numbers[numbers.Length - 1]);
+    }
+    return result;
+}
+
+//int result = 0;
+int result = Func(input);
+
+Console.WriteLine($"Part One answear: {result}");
+
+// Part Two
+
+Dictionary<string, string> literals = new Dictionary<string, string>()
+{
+    { "one", "o1e"},
+    { "two", "t2o"},
+    { "three", "t3e"},
+    { "four", "f4r"},
+    { "five", "f5e"},
+    { "six", "s6x" },
+    { "seven", "s7n" },
+    { "eight", "e8t" },
+    { "nine", "n9e" }
+};
+
+string withSpelledLetters = input;
+foreach (var key in literals.Keys)
+{
+    withSpelledLetters = withSpelledLetters.Replace(key, literals[key]);
+}
+
+result = Func(withSpelledLetters);
+
+Console.WriteLine($"Part Two answear: {result}");
---
+++ AdventOfCode/Utils/InputLoader.cs
@@ -0,0 +1,13 @@
+﻿namespace Utils
+{
+    public static class InputLoader
+    {
+        public static string LoadInput(string path = "../../../input.txt")
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
