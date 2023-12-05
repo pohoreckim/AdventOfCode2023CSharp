ea421a9ce11d957d1a4969f4293a04520dea499d
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-05 10:15:20 +0100

    Day 4 Part 1

---
+++ AdventOfCode/Day 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using Utils;
+
+// Load input
+
+string input = InputLoader.LoadInput();
+
+// Part One
+
+int result = 0;
+foreach (var line in input.Split('\n').SkipLast(1))
+{
+    string[] card = line.Substring(line.IndexOf(':') + 1).Split('|');
+    List<int> winningNumbers = new List<int>();
+    List<int> pickedNumbers = new List<int>();
+    foreach (var number in card[0].Split(' '))
+    {
+        if (int.TryParse(number, out int  winningNumber)) winningNumbers.Add(winningNumber);
+    }
+    foreach (var number in card[1].Split(' '))
+    {
+        if (int.TryParse(number, out int pickedNuber)) pickedNumbers.Add(pickedNuber);
+    }
+    int k = pickedNumbers.Intersect(winningNumbers).Count();
+    result += k > 0 ? 1 << (k - 1) : 0;
+}
+
+Console.WriteLine($"Part One answear: {result}");
+
+// Part Two
+
+
+Console.WriteLine($"Part Two answear: {result}");
