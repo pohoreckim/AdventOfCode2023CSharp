b1929440d9c2da32d4f95c6b737be9c7c5202386
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-05 10:47:34 +0100

    Day 4 Part Two

---
+++ AdventOfCode/Day 4/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4
+{
+    internal readonly record struct Card(int[] WinningNumbers, int[] PickedNumbers)
+    {
+        public int[] MatchingNumers => WinningNumbers.Intersect(PickedNumbers).ToArray();
+    }
+}
--- AdventOfCode/Day 4/Program.cs
+++ AdventOfCode/Day 4/Program.cs
@@ -1,12 +1,10 @@
-﻿using Utils;
+﻿using Day_4;
+using Utils;
 
 // Load input
 
 string input = InputLoader.LoadInput();
-
-// Part One
-
-int result = 0;
+List<Card> cards = new List<Card>();
 foreach (var line in input.Split('\n').SkipLast(1))
 {
     string[] card = line.Substring(line.IndexOf(':') + 1).Split('|');
@@ -14,13 +12,22 @@ foreach (var line in input.Split('\n').SkipLast(1))
     List<int> pickedNumbers = new List<int>();
     foreach (var number in card[0].Split(' '))
     {
-        if (int.TryParse(number, out int  winningNumber)) winningNumbers.Add(winningNumber);
+        if (int.TryParse(number, out int winningNumber)) winningNumbers.Add(winningNumber);
     }
     foreach (var number in card[1].Split(' '))
     {
         if (int.TryParse(number, out int pickedNuber)) pickedNumbers.Add(pickedNuber);
     }
-    int k = pickedNumbers.Intersect(winningNumbers).Count();
+    cards.Add(new Card(winningNumbers.ToArray(), pickedNumbers.ToArray()));
+}
+
+
+// Part One
+
+int result = 0;
+foreach (var card in cards)
+{
+    int k = card.MatchingNumers.Length;
     result += k > 0 ? 1 << (k - 1) : 0;
 }
 
@@ -28,5 +35,14 @@ Console.WriteLine($"Part One answear: {result}");
 
 // Part Two
 
+int[] cardCounts = Enumerable.Repeat(1, cards.Count).ToArray();
+for (int i = 0; i < cards.Count; i++)
+{
+    for (int j = i + 1; j <= i + cards[i].MatchingNumers.Length; j++)
+    {
+        cardCounts[j] += cardCounts[i];
+    }
+}
+result = cardCounts.Sum();
 
 Console.WriteLine($"Part Two answear: {result}");
