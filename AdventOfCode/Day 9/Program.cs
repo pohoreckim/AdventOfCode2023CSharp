751663c68259570fa5bcd21bb5e79f61e96a4adf
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-09 09:49:45 +0100

    Day 9 Part 2

--- AdventOfCode/Day 9/Program.cs
+++ AdventOfCode/Day 9/Program.cs
@@ -6,23 +6,38 @@ var lines = input.Split('\n').SkipLast(1);
 
 // Part One
 
-long Func(List<long> sequence)
+long Extrapolate(List<long> sequence)
 {
     if (sequence.All(x => x == 0)) return 0;
     List<long> diff = new List<long>();
     sequence.Aggregate((x, y) => { diff.Add(y - x); return y; });
-    return Func(diff) + sequence[sequence.Count - 1];
+    return Extrapolate(diff) + sequence[sequence.Count - 1];
 }
 
 long result = 0;
 
 foreach (var line in lines)
 {
-    result += Func(line.Trim().Split(' ').Select(x => long.Parse(x)).ToList());
+    result += Extrapolate(line.Trim().Split(' ').Select(x => long.Parse(x)).ToList());
 }
 
 Console.WriteLine($"Part One answear: {result}");
 
 // Part Two
+
+result = 0;
+
+long ExtrapolateBackwards(List<long> sequence)
+{
+    if (sequence.All(x => x == 0)) return 0;
+    List<long> diff = new List<long>();
+    sequence.Aggregate((x, y) => { diff.Add(y - x); return y; });
+    return sequence[0] - ExtrapolateBackwards(diff);
+}
+
+foreach (var line in lines)
+{
+    result += ExtrapolateBackwards(line.Trim().Split(' ').Select(x => long.Parse(x)).ToList());
+}
 
 Console.WriteLine($"Part Two answear: {result}");
