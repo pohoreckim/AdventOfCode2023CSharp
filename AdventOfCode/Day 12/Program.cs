9939766d64e29d7d74b2af2bb4dbfe584f4d14ce
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-11 10:10:46 +0100

    New projects
    New projects for each day with initial code and dependencies.

---
+++ AdventOfCode/Day 12/Program.cs
@@ -0,0 +1,16 @@
+﻿using Utils;
+// Load input
+
+string input = InputLoader.LoadInput();
+
+// Part One
+
+int result = 0;
+
+Console.WriteLine($"Part One answear: {result}");
+
+// Part Two
+
+result = 0;
+
+Console.WriteLine($"Part Two answear: {result}");
