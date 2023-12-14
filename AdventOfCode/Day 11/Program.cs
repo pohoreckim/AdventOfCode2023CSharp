d12f7a8d0ba4047fa2b19dd4f8a390b79dd67ac2
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-14 10:21:57 +0100

    Day 11 Part 1

--- AdventOfCode/Day 11/Program.cs
+++ AdventOfCode/Day 11/Program.cs
@@ -1,12 +1,44 @@
-﻿using Utils;
+﻿using Day_11;
+using Utils;
 // Load input
 
 string input = InputLoader.LoadInput();
+UniverseMap universe = new UniverseMap(input.Split('\n').SkipLast(1).ToList());
 
 // Part One
 
-int result = 0;
+List<int> emptyColumnsIds = new List<int>();
+for (int i = 0; i < universe.Width; i++)
+{
+    if (universe.GetColumn(i).All(x => x == UniverseMap.EmptySpace)) emptyColumnsIds.Add(i);
+}
+/*List<int> emptyRowsIds = new List<int>();
+for (int i = 0; i < universe.Height; i++)
+{
+    if (universe.GetRow(i).All(x => x == UniverseMap.EmptySpace)) emptyRowsIds.Add(i);
+}*/
 
+List<string> expandedMap = new List<string>();
+for (int i = 0; i < universe.Height; i++)
+{
+    var row = universe.GetRow(i);
+    for (int j = 0; j < emptyColumnsIds.Count; j++)
+    {
+        row.Insert(emptyColumnsIds[j] + j, UniverseMap.EmptySpace);
+    }
+    if (row.All(x => x == UniverseMap.EmptySpace)) expandedMap.Add(new string (row.ToArray()));
+    expandedMap.Add (new string (row.ToArray()));
+}
+
+UniverseMap expandedUniverse = new UniverseMap(expandedMap);
+ulong result = 0;
+for (int i = 0; i < expandedUniverse.Galaxies.Count; i++)
+{
+    for (int j = i + 1; j < expandedUniverse.Galaxies.Count; j++)
+    {
+        result += (ulong)(Point2D.ManhattanDistance(expandedUniverse.Galaxies[i], expandedUniverse.Galaxies[j]));
+    }
+}
 Console.WriteLine($"Part One answear: {result}");
 
 // Part Two
---
+++ AdventOfCode/Day 11/UniverseMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_11
+{
+    internal class UniverseMap
+    {
+        public const char GalaxyChar = '#';
+        public const char EmptySpace = '.';
+        char[][] _map;
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public List<Point2D> Galaxies { get; private set; }
+        public UniverseMap(List<string> lines)
+        {
+            Height = lines.Count;
+            Width = lines[0].Length;
+            Galaxies = new List<Point2D>();
+            _map = new char[Height][];
+            for (int i = 0; i < Height; i++)
+            {
+                var chars = lines[i].ToCharArray();
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    if (chars[j] == GalaxyChar) Galaxies.Add(new Point2D(j, i));
+                }
+                _map[i] = chars;
+            }
+        }
+        public List<char> GetRow(int id)
+        {
+            return _map[id].ToList();
+        }
+        public List<char> GetColumn(int id)
+        {
+            List<char> result = new List<char>();
+            for (int i = 0; i < Height; i++)
+            {
+                result.Add(_map[i][id]);
+            }
+            return result;
+        }
+    }
+}
