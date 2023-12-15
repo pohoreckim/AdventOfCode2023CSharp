e13498bed90f2a4912536c41cb45e7e4cc821f8c
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-15 16:39:13 +0100

    Day 13 Part Two

--- AdventOfCode/Day 13/Pattern.cs
+++ AdventOfCode/Day 13/Pattern.cs
@@ -53,6 +53,17 @@ namespace Day_13
             }
             return (Axis.Vertical, -1);
         }
+        public List<(Axis axis, int position)> GetMirrorsWithSmudges()
+        {
+            List<(Axis axis, int position)> result = new List<(Axis axis, int position)>();
+            List<List<int>> dims = new List<List<int>>() { _rows, _cols };
+            foreach (var dim in dims)
+            {
+                List<int> positions = FindSmudgeMirror(dim);
+                result.AddRange(positions.Select(x => (dim == _rows ? Axis.Horizontal : Axis.Vertical, x + 1)));
+            }
+            return result;
+        }
         private int FindMirror(List<int> list)
         {
             int result = -1;
@@ -69,6 +80,48 @@ namespace Day_13
                     j++;
                 }
                 if (!breakFlag) return i;
+            }
+            return result;
+        }
+        private List<int> FindSmudgeMirror(List<int> list)
+        {
+            List<int> possitions = new List<int>();
+            for (int i = 0; i < list.Count - 1; i++)
+            {
+                int j = 0;
+                bool breakFlag = false;
+                int changes = 0;
+                while (i - j >= 0 && i + j < list.Count - 1)
+                {
+                    if (list[i + 1 + j] != list[i - j])
+                    {
+                        if(changes > 0)
+                        {
+                            breakFlag = true; break;
+                        }
+                        else
+                        {
+                            int xor = list[i + 1 + j] ^ list[i - j];
+                            if (CountBits(xor) == 1)
+                            { 
+                                changes = 1; 
+                            }
+                            else { breakFlag = true; break; }
+                        }
+                    }
+                    j++;
+                }
+                if (!breakFlag) possitions.Add(i);
+            }
+            return possitions;
+        }
+        public static int CountBits(int value)
+        {
+            int result = 0;
+            while (value != 0)
+            {
+                result += value & 1;
+                value >>= 1;
             }
             return result;
         }
--- AdventOfCode/Day 13/Program.cs
+++ AdventOfCode/Day 13/Program.cs
@@ -11,17 +11,23 @@ foreach (var patternScheme in input.Split("\n\n"))
 
 // Part One
 
-int result = 0;
+List<(Axis axis, int position)> mirrors = new List<(Axis axis, int position)>();
 foreach (var pattern in patterns)
 {
-    var mirror = pattern.GetMirrorPossition();
-    result += mirror.axis == Axis.Vertical ? mirror.position : 100 * mirror.position; 
+    mirrors.Add(pattern.GetMirrorPossition());
 }
 
+int result = 0;
+result += mirrors.Select(x => x.axis == Axis.Vertical ? x.position : x.position * 100).Sum();
 Console.WriteLine($"Part One answear: {result}");
 
 // Part Two
 
 result = 0;
+for (int i = 0; i < patterns.Count; i++)
+{
+    result += patterns[i].GetMirrorsWithSmudges().Where(x => x != mirrors[i])
+        .Select(x => x.axis == Axis.Vertical ? x.position : x.position * 100).Sum();
+}
 
 Console.WriteLine($"Part Two answear: {result}");
