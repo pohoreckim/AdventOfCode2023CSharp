2e7a71e2a8a17d29db189be72161edc109310158
pohoreckim <mikolaj.pohorecki@gmail.com>
2023-12-18 15:23:37 +0100

    Point2D struct moved to Utils

---
+++ AdventOfCode/Day 16/Grid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_16
+{
+    internal class Grid
+    {
+        private char[,] _layout;
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+    }
+}
--- AdventOfCode/Utils/Point2D.cs
+++ AdventOfCode/Utils/Point2D.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day_10
+namespace Utils
 {
-    internal struct Point2D
+    public struct Point2D
     {
         public int X { get; private set; }
         public int Y { get; private set; }
@@ -18,5 +18,6 @@ namespace Day_10
         public static bool operator ==(Point2D a, Point2D b) => a.X == b.X && a.Y == b.Y;
         public static bool operator !=(Point2D a, Point2D b) => !(a == b);
         public static Point2D operator +(Point2D a, Point2D b) => new Point2D(a.X + b.X, a.Y + b.Y);
+        public static int ManhattanDistance(Point2D left, Point2D right) => Math.Abs(left.X - right.X) + Math.Abs(left.Y - right.Y);
     }
 }
